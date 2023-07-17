92da33ca9ed21d4ce2981fb6c97ab5a5e0284867
Gritmax <test@test.com>
2023-07-17 23:57:43 +0300

    Setting up asset management in Laravel

---
+++ src/Providers/AssetsServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+/*3*/
+namespace Xamtirg\Assets\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * @since 22/07/2023 11:23 PM
+ */
+class AssetsServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../../config/assets.php', 'assets');
+        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'assets');
+
+        if ($this->app->runningInConsole()) {
+            $this->publishes([__DIR__ . '/../../config/assets.php' => config_path('assets.php')], 'config');
+            $this->publishes([__DIR__ . '/../../resources/views' => resource_path('views/vendor/assets')], 'views');
+        }
+    }
+}
